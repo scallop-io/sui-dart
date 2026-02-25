e959be9cfbbf83e19c9e8a4a86904ff0795ee4e8
Jovin <jovinlidan1@gmail.com>
2026-02-25 12:04:12 +0700

    chore: update Sui URLs to include port 443 and upgrade SDK version to 3.11.0

--- lib/sui_urls.dart
+++ lib/sui_urls.dart
@@ -4,12 +4,12 @@ import 'package:meta/meta.dart';
 final class SuiUrls {
   const SuiUrls._();
 
-  static const String mainnet = "https://fullnode.mainnet.sui.io";
-  static const String testnet = "https://fullnode.testnet.sui.io";
-  static const String devnet = "https://fullnode.devnet.sui.io";
+  static const String mainnet = "https://fullnode.mainnet.sui.io:443";
+  static const String testnet = "https://fullnode.testnet.sui.io:443";
+  static const String devnet = "https://fullnode.devnet.sui.io:443";
 
-  static const String faucetDev = "https://faucet.devnet.sui.io";
-  static const String faucetTest = "https://faucet.testnet.sui.io";
+  static const String faucetDev = "https://faucet.devnet.sui.io:443";
+  static const String faucetTest = "https://faucet.testnet.sui.io:443";
 
   static const String webSocketMainnet = 'wss://fullnode.mainnet.sui.io';
   static const String webSocketTestnet = 'wss://fullnode.testnet.sui.io';
